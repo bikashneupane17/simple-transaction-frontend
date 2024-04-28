e7f33911b149e80a9d6e81349029bb71f0ec4803
bikashneupane17 <edubikkash@gmail.com>
2024-04-29 09:58:26 +1000

    FrontEnd CleanUp

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+// import Button from "react-bootstrap/Button"; //maybe 3kb
+import { Button } from "react-bootstrap"; //maybe 300kb
+
+function App() {
+  return <>Client</>;
+}
+
+export default App;
